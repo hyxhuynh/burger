42c0be8c339a1a2bfd50a73cb928f74356d989c2
Hy Huynh <hyxhuynh@gmail.com>
2018-11-18 01:04:23 -0800

    completed app

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,16 @@
 --      * **id**: an auto incrementing int that serves as the primary key.
 --      * **burger_name**: a string.
 --      * **devoured**: a boolean.
+
+-- Schema
+-- DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,8 @@
 -- write insert queries to populate the `burgers` table with at least three entries.
+
+INSERT INTO burgers (burger_name) VALUES ('Prime Beef Burger 1/3 Pound');
+INSERT INTO burgers (burger_name) VALUES ('American Wagyu 1/2 Pound');
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Double Cheeseburger Love', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Bacon Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Paleo Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('House Made Veggie Burger');
